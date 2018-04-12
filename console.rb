3af75535bc043742ad25de755fc194afafa6d165
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-04-12 15:58:02 +0100

    film save method

--- console.rb
+++ console.rb
@@ -6,25 +6,38 @@ require('pry-byebug')
 
 customer1 = Customer.new({
   'name' => 'Lesley Knope',
-  'wallet' => 50
+  'wallet' => 50.00
   })
 
   customer1.save()
 
 customer2 = Customer.new({
     'name' => 'Ronald Ulysees Swanson',
-    'wallet' => 10000
+    'wallet' => 10000.00
     })
 
 customer2.save()
 
 customer3 = Customer.new({
   'name' => 'April Ludgate-Dwyer',
-  'wallet' => 20
+  'wallet' => 20.00
   })
 
   customer3.save()
 
+  film1 = Film.new ({
+    'title' => 'Bring It On',
+    'price' => 5.00
+    })
+film1.save()
+
+film2 = Film.new({
+  'title' => 'Django Unchained',
+  'price' => 6.00
+
+  })
+
+  film2.save()
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -2,7 +2,7 @@ require_relative('../db/sql_runner.rb')
 
 class Film
 attr_reader :id
-attr_accessor :title, :price 
+attr_accessor :title, :price
 def initialize(options)
   @id = options['id'].to_i
   @title = options['title']
@@ -10,6 +10,14 @@ def initialize(options)
 
 end
 
+
+def save()
+  sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id;"
+  values = [@title, @price]
+  film = SqlRunner.run(sql, values).first
+  @id = film['id'].to_i
+
+end
 
 
 end
